0acfdb04be40a55655150107618bb0198413de38
Dure Sameen <dure.sameen@wsite.nl>
2020-09-12 20:14:33 +0200

    Add project files.

---
+++ Class1WebApi/Controllers/CandidateController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class1WebApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class1WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+
+        private static IList<ICandidate> candidateList = new List<ICandidate>();
+        // GET: api/Candidate
+        [HttpGet]
+        public IEnumerable<ICandidate> Get()
+        {
+            return candidateList;
+        }
+
+        // GET: api/Candidate/5
+        [HttpGet("{id}", Name = "Get")]
+        public ICandidate Get(int id)
+        {
+            return candidateList.Where(c => c.CandidateId == id).FirstOrDefault();
+        }
+
+        // POST: api/Candidate
+        [HttpPost]
+        public void Post([FromBody] Candidate candidate)
+        {
+            candidateList.Add(candidate);
+        }
+
+        // PUT: api/Candidate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Candidate candidate)
+        {
+            var candidateByList = candidateList.Where(c => c.CandidateId == id).FirstOrDefault();
+            candidateByList.FirstName = candidate.FirstName;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           var candidate= candidateList.Where(c => c.CandidateId == id).FirstOrDefault();
+            candidateList.Remove(candidate);
+        }
+    }
+}
---
+++ Class1WebApi/Data/Candidate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Class1WebApi.Data
+{
+   public class Candidate : ICandidate
+    {
+        public string _myname;
+        public int  CandidateId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+
+                if (value < 13)
+                    throw new Exception(" too young");
+                else _age = value;
+            }
+        }
+        public bool IsAvailable { get; set; }
+
+        public Candidate()
+        {
+            FirstName = "firstname";
+            LastName = "LastName";
+            Age = 13;
+            IsAvailable = true;
+
+
+        }
+
+        public Candidate(int id, string firstName, string lastname, int age, bool isAvailable)
+        {
+            FirstName = firstName;
+            LastName = lastname;
+            Age = age;
+            IsAvailable = isAvailable;
+            CandidateId = id;
+
+
+        }
+        public void ReturnNothing()
+        { }
+
+        public string GetFullName()
+        { return FirstName + " " + LastName; }
+    }
+}
